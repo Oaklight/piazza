6b5d641994407822e6f772da1638abfccb7c60c9
Peng Ding <pding.dp@foxmail.com>
2026-07-08 13:45:00 +0200

    chore: bump version to 0.2.2

--- src/piazza/__init__.py
+++ src/piazza/__init__.py
@@ -1,6 +1,6 @@
 """piazza - Agent messaging hub for multi-agent collaboration."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from piazza.backends import MemoryBackend, SQLiteBackend
 from piazza.bus import Bus, SQLiteBus
